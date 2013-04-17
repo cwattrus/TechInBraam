fcf635b7b3442895e2b404d40f057cdfffa44384
Craig Wattrus <cwattrus@gmail.com>
2013-04-17 09:41:46 +0200

    Craig: Fixing the background images so they look ok on smaller screens

--- js/script.js
+++ js/script.js
@@ -72,56 +72,56 @@ $(document).ready(function(){
     });
 
 	
-	// For each element that has a data-type attribute
-	$('section[data-type="background"]').each(function(){
+	// // For each element that has a data-type attribute
+	// $('section[data-type="background"]').each(function(){
 	
 	
-		// Store some variables based on where we are
-		var $self = $(this),
-			offsetCoords = $self.offset(),
-			topOffset = offsetCoords.top;
+	// 	// Store some variables based on where we are
+	// 	var $self = $(this),
+	// 		offsetCoords = $self.offset(),
+	// 		topOffset = offsetCoords.top;
 		
-		// When the window is scrolled...
-	    $(window).scroll(function() {
+	// 	// When the window is scrolled...
+	//     $(window).scroll(function() {
 	
-			// If this section is in view
-			if ( ($window.scrollTop() + $window.height()) > (topOffset) &&
-				 ( (topOffset + $self.height()) > $window.scrollTop() ) ) {
+	// 		// If this section is in view
+	// 		if ( ($window.scrollTop() + $window.height()) > (topOffset) &&
+	// 			 ( (topOffset + $self.height()) > $window.scrollTop() ) ) {
 	
-				// Scroll the background at var speed
-				// the yPos is a negative value because we're scrolling it UP!								
-				var yPos = -($window.scrollTop() / $self.data('speed')); 
+	// 			// Scroll the background at var speed
+	// 			// the yPos is a negative value because we're scrolling it UP!								
+	// 			var yPos = -($window.scrollTop() / $self.data('speed')); 
 				
-				// If this element has a Y offset then add it on
-				if ($self.data('offsetY')) {
-					yPos += $self.data('offsetY');
-				}
+	// 			// If this element has a Y offset then add it on
+	// 			if ($self.data('offsetY')) {
+	// 				yPos += $self.data('offsetY');
+	// 			}
 				
-				// Put together our final background position
-				var coords = '100% '+ (yPos*0.5) + 'px';
+	// 			// Put together our final background position
+	// 			var coords = '100% '+ (yPos*0.5) + 'px';
 
-				// Move the background
-				$self.css({ backgroundPosition: coords });
+	// 			// Move the background
+	// 			$self.css({ backgroundPosition: coords });
 				
-				// Check for other sprites in this section	
-				$('[data-type="sprite"]', $self).each(function() {
+	// 			// Check for other sprites in this section	
+	// 			$('[data-type="sprite"]', $self).each(function() {
 					
-					// Cache the sprite
-					var $sprite = $(this);
+	// 				// Cache the sprite
+	// 				var $sprite = $(this);
 					
-					// Use the same calculation to work out how far to scroll the sprite
-					var yPos = -($window.scrollTop() / $sprite.data('speed'));					
-					var coords = $sprite.data('Xposition') + ' ' + (yPos + $sprite.data('offsetY')) + 'px';
+	// 				// Use the same calculation to work out how far to scroll the sprite
+	// 				var yPos = -($window.scrollTop() / $sprite.data('speed'));					
+	// 				var coords = $sprite.data('Xposition') + ' ' + (yPos + $sprite.data('offsetY')) + 'px';
 					
-					$sprite.css({ backgroundPosition: coords });													
+	// 				$sprite.css({ backgroundPosition: coords });													
 					
-				}); // sprites
+	// 			}); // sprites
 			
 			
-			}; // in view
+	// 		}; // in view
 	
-		}); // window scroll
+	// 	}); // window scroll
 			
-	});	// each data-type
+	// });	// each data-type
 
 }); // document ready
